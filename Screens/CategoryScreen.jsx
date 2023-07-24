3437f1d06e2797480c8479279249804abb5748c4
lit <103055119+EricaAnnor@users.noreply.github.com>
2023-07-24 16:55:34 +0000

    added cartegory data 1

--- Screens/CategoryScreen.jsx
+++ Screens/CategoryScreen.jsx
@@ -2,3 +2,10 @@ import React from 'react';
 import { View, StyleSheet, FlatList, Text } from 'react-native';
 
 
+const categoryData = [
+    {
+        id: 1,
+        categoryName: 'Breakfast',
+        image: require('../assets/cg-breakfast.png'),
+    },
+
