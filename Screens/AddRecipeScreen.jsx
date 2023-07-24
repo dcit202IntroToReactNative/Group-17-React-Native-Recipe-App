5ecdf9e15ad6c6a6dbc147b45c28d9f5818f8c76
S-yartey007 <104656863+S-yartey007@users.noreply.github.com>
2023-07-24 18:34:52 +0000

    Update AddRecipeScreen.jsx with styles

--- Screens/AddRecipeScreen.jsx
+++ Screens/AddRecipeScreen.jsx
@@ -2,3 +2,71 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingTop: 10,
+        justifyContent: "center"
+    },
+    titleContainer: {
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
