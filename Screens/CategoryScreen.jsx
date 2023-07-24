d90557e44076eb1a449382f96d9b2644deaab93f
Julia Osei <102672895+JuliaOse@users.noreply.github.com>
2023-07-24 17:12:03 +0000

    Update CategoryScreen.jsx

--- Screens/CategoryScreen.jsx
+++ Screens/CategoryScreen.jsx
@@ -19,3 +19,17 @@ const CategoryScreen = () => {
     const renderItem = ({ item }) => (
         <CategoryCard Name={item.categoryName} image={item.image} />
     );
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Text style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 30,
+                fontWeight: 'bold',
+                paddingTop: 100,
+                marginBottom: 10
+            }}>
